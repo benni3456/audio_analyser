7f17a7cf9b53fa148ec3dc216c46b051aa8c7452
Benjamin Lau <benjamin.lau@student.jade-hs.de>
2013-06-10 16:00:30 +0200

    minimal PEP

--- gain_plotter.py
+++ gain_plotter.py
@@ -10,17 +10,18 @@ from sound_device import SAMPLING_RATE as fs
 
 class GainPlotter:
     def __init__(self, PlotSpek, audiobuffer):
+        '''  '''
         self.wholestream = zeros(1)
         self.PlotSpek = PlotSpek
         self.audiobuffer = audiobuffer
         
         
     def plot(self):
-        
-        data = self.audiobuffer.newdata()
         ''' function to plot the level over time '''
+        data = self.audiobuffer.newdata()        
         r_m_s = dB(rms(data))
-    # recalls function gain_meter, so therefore computes the level in dB FS
+        
+        # recalls function gain_meter, so therefore computes the level in dB FS
         self.wholestream = concatenate([self.wholestream,[r_m_s]])
         self.PlotSpek.axes.plot(self.wholestream[-10:])
         self.PlotSpek.axes.set_ylim(-100,0)
