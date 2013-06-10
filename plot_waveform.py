db26d723508c45386249fc30146ad0b748ac7697
Oliver Buttler <oliver.buttler@student.jade-hs.de>
2013-06-10 14:38:50 +0200

    modified pep8

--- plot_waveform.py
+++ plot_waveform.py
@@ -14,8 +14,10 @@ from pylab import *
 
 class plot_waveform(QtGui.QWidget):
     """
+    Draw a plot for waveform (Oszilloskop) for the current time
 
-
+    The plot is drawn centered on the widget with smaller width/height as the
+    widget itself. If the widget is not square, the smaller dimension is used.
     """
 
 
@@ -25,7 +27,7 @@ class plot_waveform(QtGui.QWidget):
         self.setWindowTitle('graphic')
         self.y_range = 2
         self.y_ticks = [-1, 0, 1]
-        self.sidespace = 50
+        self.side_space = 50
         self.y_step = 3
 
     def readArray(self, amplitude, timeValue):
@@ -37,65 +39,63 @@ class plot_waveform(QtGui.QWidget):
 
     def draw_text(self, painter):
         painter.drawText(QtCore.QRectF(-30,
-                        -(self.height()-2 * self.sidespace) / 2-20, 40, 20),
+                        -(self.height()-2 * self.side_space) / 2-20, 40, 20),
                          QtCore.Qt.AlignCenter, 'signal')
-        painter.drawText(QtCore.QRectF(self.width() - self.sidespace * 2,
-                        (self.height()-2 * self.sidespace) / 2, 20, 20),
+        painter.drawText(QtCore.QRectF(self.width() - self.side_space * 2,
+                        (self.height()-2 * self.side_space) / 2, 20, 20),
                          QtCore.Qt.AlignCenter, 'time')
         start_point = 0
-        x_stepsize = (self.width() - self.sidespace * 2) / \
-                     (len(self.timeValue))
+        x_step_size = ((self.width() - self.side_space * 2) /
+                     (len(self.timeValue)))
         for i in range(0, len(self.timeValue), 1):
-            # x-Achse Beschriftung
-            painter.drawText(QtCore.QRectF(start_point - x_stepsize / 2,
-                            (self.height() - 2 * self.sidespace) / 2,
-                            x_stepsize, 20),
+            # label x-axis
+            painter.drawText(QtCore.QRectF(start_point - x_step_size / 2,
+                            (self.height() - 2 * self.side_space) / 2,
+                            x_step_size, 20),
                              QtCore.Qt.AlignCenter, str(self.timeValue[i]))
-            start_point = start_point + (self.width() - self.sidespace * 2) \
-                          / (len(self.timeValue))
-        y_axis = (self.height()-2 * self.sidespace) / 2
+            start_point = (start_point + (self.width() - self.side_space * 2) /
+                          (len(self.timeValue)))
+        y_axis = (self.height()-2 * self.side_space) / 2
         painter.drawText(QtCore.QRectF(-30, y_axis - 25, 20, 20),
                          QtCore.Qt.AlignCenter, 'min')
-        y_axis = y_axis - (self.height() - self.sidespace * 2) / \
-                 (self.y_step - 1)
-        # y achse beschriftung
+        y_axis = (y_axis - (self.height() - self.side_space * 2) /
+                 (self.y_step - 1))
+        # label y-axis
         painter.drawText(QtCore.QRectF(-30, y_axis - 10, 20, 20),
                          QtCore.Qt.AlignCenter, str(self.y_ticks[1]))
-        y_axis = y_axis - (self.height() - self.sidespace * 2) / \
-                 (self.y_step - 1)
+        y_axis = (y_axis - (self.height() - self.side_space * 2) /
+                 (self.y_step - 1))
         painter.drawText(QtCore.QRectF(-30, y_axis + 5, 20, 20),
                          QtCore.Qt.AlignCenter, 'max')
 
     def draw_ticks(self, painter):
         painter.save()
-        # Skalieren auf Anzahl der Werte
-        painter.scale(((self.width() - self.sidespace * 2) /
+        # scale number of value
+        painter.scale(((self.width() - self.side_space * 2) /
                        (len(self.timeValue))), 1)
         painter.setPen(QtGui.QPen(QtGui.QBrush(QtCore.Qt.black), 0))
-        #Schleife für x-Achse
         for a in range(0, len(self.amplitude) - 1):
-            #x achse einheiten striche
+            # draw ticks of x-axis
             painter.drawLine(QtCore.QLineF(a + 1, (self.height() - 2 *
-                                                   self.sidespace) / 2,
+                                                   self.side_space) / 2,
                                            a + 1, (self.height() - 2 *
-                                                   self.sidespace) / 2 + 3))
+                                                   self.side_space) / 2 + 3))
         painter.restore()   
         painter.save()
-        # Skalieren auf Anzahl der Werte
-        painter.scale(1, -(self.height() - 2 * self.sidespace - 30) / 2)
+        # scale number of value
+        painter.scale(1, -(self.height() - 2 * self.side_space - 30) / 2)
         painter.setPen(QtGui.QPen(QtGui.QBrush(QtCore.Qt.black), 0))
-        # y achse einheiten striche
+        # draw ticks of y-axis
         for b in (-1, 0, 1):
             painter.drawLine(QtCore.QLineF(-5, b, 0, b))
         painter.restore()
 
     def draw_data(self, painter):
         painter.save()
-        # Skalieren auf Anzahl der Werte
-        painter.scale(((self.width() - self.sidespace * 2) /
+        painter.scale(((self.width() - self.side_space * 2) /
                        (len(self.timeValue))),
-                      -((self.height() - self.sidespace * 2 - 30) /
-                        self.y_range))
+                      - ((self.height() - self.side_space * 2 - 30) /
+                         self.y_range))
         painter.setPen(QtGui.QPen(QtGui.QBrush(QtCore.Qt.black), 0))
         brush = QtGui.QBrush(QtCore.Qt.blue)
         painter.setBrush(brush)
@@ -116,20 +116,20 @@ class plot_waveform(QtGui.QWidget):
         pen = QtGui.QPen(QtCore.Qt.black, 1, QtCore.Qt.SolidLine)
         brush = QtGui.QBrush(QtCore.Qt.white)
         painter.setBrush(brush)
-        painter.drawRect(self.sidespace, self.sidespace,
-                         self.width() - self.sidespace * 2,
-                         self.height() - self.sidespace * 2)
+        painter.drawRect(self.side_space, self.side_space,
+                         self.width() - self.side_space * 2,
+                         self.height() - self.side_space * 2)
         painter.setBrush(QtGui.QBrush())
         painter.setPen(pen)
-        # x-Axis frequency
-        painter.drawLine(self.sidespace, self.sidespace, self.sidespace,
-                         self.height() - self.sidespace)
-        # y-Axis dB
-        painter.drawLine(self.sidespace, self.height() - self.sidespace,
-                         self.width() - self.sidespace,
-                         self.height() - self.sidespace)
-        # Koordinatensystem auf 0 Punkt der Grafik
-        painter.translate(self.sidespace, self.height() / 2)
+        # x-axis time
+        painter.drawLine(self.side_space, self.side_space, self.side_space,
+                         self.height() - self.side_space)
+        # y-axis signal
+        painter.drawLine(self.side_space, self.height() - self.side_space,
+                         self.width() - self.side_space,
+                         self.height() - self.side_space)
+        # point zero of the coordinate system on graph
+        painter.translate(self.side_space, self.height() / 2)
         self.draw_text(painter)
         self.draw_ticks(painter)
         self.draw_data(painter)
